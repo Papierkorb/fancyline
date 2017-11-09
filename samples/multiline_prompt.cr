820097b39eca6caa831b73ca89fa7e1ac1e610b3
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-11-09 20:51:08 +0100

    Add  samples/multiline_prompt.cr, showcasing multi-line prompts

---
+++ samples/multiline_prompt.cr
@@ -0,0 +1,18 @@
+require "../../src/fancyline"
+
+# Demonstration of multi-line prompts.
+
+fancy = Fancyline.new # Build a shell object
+puts "Press Ctrl-C or Ctrl-D to quit."
+
+loop do
+  top_bar = "═" * (fancy.tty.columns - 2)
+
+  lprompt = "╔#{top_bar}╗\r\n╚═══ #{"❱❱❱".colorize(:yellow)} "
+  rprompt = "#{"❰❰❰".colorize(:yellow)} ═══╝"
+
+  input = fancy.readline(lprompt, rprompt: rprompt)
+
+  break if input.nil?
+  puts "Got #{input}"
+end
