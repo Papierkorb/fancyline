093a48117467adcde2560549bb4ff4bc7e7b688e
iain barnett <helpful-iain@theprintedbird.com>
2021-06-28 16:52:36 +0900

    Fixed sample, stdlib logger is Log etc.

--- samples/concurrent_output.cr
+++ samples/concurrent_output.cr
@@ -1,19 +1,18 @@
 require "../src/fancyline"
-require "logger"
+require "log"
 
 # This sample demonstrates `Fancyline#grab_output` to output log lines while the
 # user is able to use the prompt like normal.
 
 fancy = Fancyline.new
-logger = Logger.new STDOUT
 
-logger.info "Hit Ctrl-D or Ctrl-C to end the demo."
+Log.info {  "Hit Ctrl-D or Ctrl-C to end the demo." }
 
 # Simulate a background task writing something onto standard output.
 spawn do
   0.upto(Int64::MAX).each do |i|
-    fancy.grab_output do                    # Grab the output
-      logger.info "Running #{i} seconds..." # Print something
+    fancy.grab_output do # Grab the output
+      Log.info {  "Running #{i} seconds..." } # Print something
     end
 
     sleep 1
@@ -23,5 +22,5 @@ end
 # Prompt the user like normal
 while line = fancy.readline("Command> ")
   # You don't have to grab here, though it wouldn't break anything!
-  logger.info "Got command #{line.inspect}"
+  Log.info {  "Got command #{line.inspect}" }
 end
